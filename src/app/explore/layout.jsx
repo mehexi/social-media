d4b0ad738e732ff5143740a1a44d097cfa036a57
mehedi hasan <126792834+mehexi@users.noreply.github.com>
2024-12-11 01:09:27 +0600

    Changed follow feed to the children on (feed)

--- src/app/explore/layout.jsx
+++ src/app/explore/layout.jsx
@@ -1,11 +1,14 @@
+import MobileNav from "@/components/sideBar/MobileNav";
 import SideBar from "@/components/sideBar/SideBar";
 
 export default function ExploreLayout({ children }) {
   return (
-    <section className="grid grid-cols-12">
+    <section className="grid grid-cols-12 max-w-5xl mx-auto">
       <SideBar />
-      <div className="col-span-8 border">{children}</div>
-      <div className="border col-span-2"></div>
+      <div className="col-span-8 max-md:col-span-11 max-sm:col-span-12  border-r">
+        {children} <MobileNav/>
+      </div>
+      <div className="col-span-2"></div>
     </section>
   );
 }

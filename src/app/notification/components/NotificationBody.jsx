4e34579ebeabaaabe05a892f7460086e17c1d633
mehedi hasan <126792834+mehexi@users.noreply.github.com>
2024-12-19 20:38:10 +0600

    check on vercel

--- src/app/notification/components/NotificationBody.jsx
+++ src/app/notification/components/NotificationBody.jsx
@@ -5,13 +5,13 @@ import React from "react";
 const NotificationBody = ({ notifications, currentUser }) => {
   const liveNotifications = useNotifications(currentUser.id);
 
-  console.log(liveNotifications);
+  console.log('');
 
   if (notifications.length === 0) {
     return <h1>no notifications</h1>;
   }
 
   return <>NotificationBody</>;
-};
+};  
 
 export default NotificationBody;
